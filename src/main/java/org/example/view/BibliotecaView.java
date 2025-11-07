89a17a9e8424a26814303c2815d6d7c857aa0a71
bruno_l_medeiros <brunoluismedeiros.08@gmail.com>
2025-11-06 21:28:28 -0300

    ?

--- src/main/java/org/example/view/BibliotecaView.java
+++ src/main/java/org/example/view/BibliotecaView.java
@@ -1,19 +1,29 @@
 package org.example.view;
 
+import org.example.model.Livro;
+import org.example.model.Usuario;
+import org.example.service.EmprestimoService;
 import org.example.service.LivroService;
+import org.example.service.UsuarioService;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class BibliotecaView {
     private final Scanner sc;
     private final LivroService livroService;
+    private final UsuarioService usuarioService;
+    private final EmprestimoService emprestimoService;
 
-    public BibliotecaView(Scanner sc,LivroService livroService) {
+    public BibliotecaView(Scanner sc,LivroService livroService,UsuarioService usuarioService, EmprestimoService emprestimoService) {
         this.sc = sc;
         this.livroService = livroService;
+        this.usuarioService = usuarioService;
+        this.emprestimoService = emprestimoService;
     }
 
     public boolean mostrarMenu() {
+        System.out.println("\n\n------ BIBLIOTECA ------");
         System.out.println("[1] Cadastrar Livro");
         System.out.println("[2] Cadastrar Usuario");
         System.out.println("[3] Consultar Todos os Livros Cadastrados");
@@ -34,8 +44,10 @@ public class BibliotecaView {
                 inserirUsuario();
                 break;
             case 3:
+                listarLivro();
                 break;
             case 4:
+                inserirEmprestimo();
                 break;
             case 5:
                 break;
@@ -50,15 +62,66 @@ public class BibliotecaView {
     }
 
     private void inserirLivro() {
-        System.out.println("Digite o Nome do Livro: ");
+        System.out.print("Digite o Nome do Livro: ");
         String livro = sc.nextLine();
 
-        System.out.println("Digite o Autor do Livro: ");
+        System.out.print("Digite o Autor do Livro: ");
         String autor = sc.nextLine();
 
-        System.out.println("Digite o Ano do Livro: ");
+        System.out.print("Digite o Ano do Livro: ");
         int ano = sc.nextInt();
 
         livroService.inserirLivro(livro,autor,ano);
+    }
+
+    private void inserirUsuario() {
+        System.out.print("Digite o Nome do Usuario: ");
+        String nome = sc.nextLine();
+
+        System.out.print("Digite o Email do Usuario");
+        String email = sc.nextLine();
+
+        usuarioService.inserirUsuario(nome,email);
+    }
+
+    private void listarLivro() {
+        List<Livro> livroList = livroService.listarLivro();
+
+        livroList.forEach(livro -> {
+            System.out.printf("ID: %-5d | Título: %-20s | Autor: %-20s | Ano do Livro: %-5d | Disponivel: %-15s%n",
+                    livro.getIdLivro(),
+                    livro.getTitulo(),
+                    livro.getAutor(),
+                    livro.getAno(),
+                    livro.isDisponivel());
+        });
+    }
+
+    private void listarUsuario() {
+        List<Usuario> usuarioList = usuarioService.listarUsuario();
+
+        usuarioList.forEach(usuario -> {
+            System.out.printf("ID: %-5d | Nome: %-20s | Email: %-20s ",
+                    usuario.getIdUsuario(),
+                    usuario.getNome(),
+                    usuario.getEmail());
+        });
+
+    }
+
+    private void inserirEmprestimo() {
+        listarLivro();
+        System.out.print("\nInsira o Identificador do Livro: ");
+        int idLivro = sc.nextInt();
+
+        listarUsuario();
+        System.out.print("\nInsira o Identificador do Usuario: ");
+        int idUsuario = sc.nextInt();
+
+        try {
+            emprestimoService.inserirEmprestimo(idLivro, idUsuario);
+        } finally {
+            livroService.atualizarStatusLivro(idLivro);
+        }
     }
 }
